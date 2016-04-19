389aa3bc9f0d29097b244534ef7feab0bae9e617
Arturo Rivas <arturo.rivas@icloud.com>
2016-04-19 10:41:22 +0200

    Falta la pantalla de detalles
    Añadidos:
    - Controlar que el ISBN no estuviera ya
    - Borrar entradas

--- Mis libros/MasterViewController.swift
+++ Mis libros/MasterViewController.swift
@@ -9,19 +9,34 @@
 import UIKit
 import CoreData
 
+struct Libro {
+    var título: String
+    var autor: String
+    var portada: UIImage?
+    var isbn: String
+    
+    init(título: String, autor: String, portada: UIImage?, isbn: String) {
+        self.título = título
+        self.autor = autor
+        self.portada = portada
+        self.isbn = isbn
+    }
+}
+
 class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 
     var detailViewController: DetailViewController? = nil
     var managedObjectContext: NSManagedObjectContext? = nil
-
+    
+    var colección = [Libro]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(MasterViewController.insertNewObject(_:)))
-        self.navigationItem.rightBarButtonItem = addButton
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(MasterViewController.insertNewObject(_:)))
+        //self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -38,7 +53,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
+    /*func insertNewObject(sender: AnyObject) {
         let context = self.fetchedResultsController.managedObjectContext
         let entity = self.fetchedResultsController.fetchRequest.entity!
         let newManagedObject = NSEntityDescription.insertNewObjectForEntityForName(entity.name!, inManagedObjectContext: context)
@@ -56,6 +71,77 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
             //print("Unresolved error \(error), \(error.userInfo)")
             abort()
         }
+    }*/
+    
+    @IBAction func insertarNuevoLibro(sender: AnyObject) {
+        var campoTexto: UITextField!
+        
+        let alerta = UIAlertController(title: "Libros", message: "Busque su libro para ver los detalles y añadirlo a la lista", preferredStyle: .Alert)
+        alerta.addTextFieldWithConfigurationHandler({(textField: UITextField) in
+            textField.placeholder = "Introduca ISBN"
+            campoTexto = textField
+        })
+        alerta.addAction(UIAlertAction(title: "Buscar", style: UIAlertActionStyle.Default, handler: {(action) in
+            self.obtenerLibro(campoTexto.text!)
+            self.tableView.reloadData()
+            self.performSegueWithIdentifier("showDetail", sender: nil)
+        }))
+        
+        presentViewController(alerta, animated: true, completion: nil)
+    }
+    
+    func obtenerLibro(isbn: String) {
+        // TODO: Controlar que ese isbn no esté ya (mostrar alert y descartar)
+        
+        let url = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + isbn)
+        
+        do {
+            let respuesta = try NSJSONSerialization.JSONObjectWithData(NSData(contentsOfURL: url!)!, options: .MutableLeaves) as? [String: AnyObject]
+            
+            var losAutores: String = ""
+            var elTítulo: String = ""
+            var laImagen: UIImage?
+            
+            if respuesta != nil && !respuesta!.isEmpty {
+                
+                var autores = respuesta!["ISBN:\(isbn)"]!["authors"] as? [[String: String]]
+                if autores != nil {
+                    
+                    losAutores = autores![0]["name"]!
+                    autores?.removeAtIndex(0)
+                    
+                    for autor in autores! {
+                        losAutores = "\(losAutores), \(autor["name"]!)"
+                    }
+                }
+                
+                let título = respuesta!["ISBN:\(isbn)"]!["title"] as? String
+                if título != nil {
+                    elTítulo = título!
+                }
+                
+                let cover = respuesta!["ISBN:\(isbn)"]!["cover"] as? [String: String]
+                
+                if cover != nil {
+                    let imagenUrl = cover!["medium"]! as String
+                    
+                    laImagen = UIImage(data: NSData(contentsOfURL: NSURL(string: imagenUrl)!)!)
+                } else {
+                    laImagen = nil
+                }
+                
+                colección.append(Libro(título: elTítulo, autor: losAutores, portada: laImagen, isbn: isbn))
+            } else {
+                let alert = UIAlertController(title: "Error", message: "No hay ningún libro con ese ISBN", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Aceptar", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+            
+        } catch {
+            let alert = UIAlertController(title: "Error", message: "Hay problemas con la conexión a Internet. Inténtelo de nuevo más tarde.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Aceptar", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
 
     // MARK: - Segues
@@ -63,30 +149,40 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-            let object = self.fetchedResultsController.objectAtIndexPath(indexPath)
+                //let object = self.fetchedResultsController.objectAtIndexPath(indexPath)
+                let object = self.colección[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
+        } else {
+            let object = self.colección.last
+            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+            controller.detailItem = object
+            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+            controller.navigationItem.leftItemsSupplementBackButton = true
         }
     }
 
     // MARK: - Table View
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return self.fetchedResultsController.sections?.count ?? 0
+        //return self.fetchedResultsController.sections?.count ?? 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionInfo = self.fetchedResultsController.sections![section]
-        return sectionInfo.numberOfObjects
+        /*let sectionInfo = self.fetchedResultsController.sections![section]
+        return sectionInfo.numberOfObjects*/
+        return colección.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        let object = self.fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
-        self.configureCell(cell, withObject: object)
+        /*let object = self.fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
+        self.configureCell(cell, withObject: object)*/
+        cell.textLabel?.text = colección[indexPath.row].título
         return cell
     }
 
@@ -96,7 +192,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     }
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
+        /*if editingStyle == .Delete {
             let context = self.fetchedResultsController.managedObjectContext
             context.deleteObject(self.fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject)
                 
@@ -108,16 +204,16 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
                 //print("Unresolved error \(error), \(error.userInfo)")
                 abort()
             }
-        }
+        }*/
     }
 
-    func configureCell(cell: UITableViewCell, withObject object: NSManagedObject) {
+    /*func configureCell(cell: UITableViewCell, withObject object: NSManagedObject) {
         cell.textLabel!.text = object.valueForKey("timeStamp")!.description
-    }
+    }*/
 
     // MARK: - Fetched results controller
 
-    var fetchedResultsController: NSFetchedResultsController {
+    /*var fetchedResultsController: NSFetchedResultsController {
         if _fetchedResultsController != nil {
             return _fetchedResultsController!
         }
@@ -184,7 +280,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     func controllerDidChangeContent(controller: NSFetchedResultsController) {
         self.tableView.endUpdates()
-    }
+    }*/
 
     /*
      // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
